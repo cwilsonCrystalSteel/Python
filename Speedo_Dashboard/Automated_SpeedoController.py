df5de12cebef315b8d747c7966f5fb4ef9ee61dd
cwilsonCrystalSteel <127119558+cwilsonCrystalSteel@users.noreply.github.com>
2023-03-13 08:22:18 -0500

    Automation continuation

--- Speedo_Dashboard/Automated_SpeedoController.py
+++ Speedo_Dashboard/Automated_SpeedoController.py
@@ -4,6 +4,8 @@ Created on Mon Mar  6 19:37:10 2023
 
 @author: CWilson
 """
+import sys
+sys.path.append("C:\\Users\\cwilson\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python39\\site-packages")
 
 from Pull_Fablisting_data import get_fablisting_plus_model_summary
 from Pull_TimeClock_data import get_timeclock_summary
