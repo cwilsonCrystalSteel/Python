99c8057573a92e7911e76e0f9559f6cc77677a36
cwilsonCrystalSteel <127119558+cwilsonCrystalSteel@users.noreply.github.com>
2023-03-16 09:59:51 -0500

    More good updates for timing of things

--- Speedo_Dashboard/Automated_SpeedoController.py
+++ Speedo_Dashboard/Automated_SpeedoController.py
@@ -7,15 +7,30 @@ Created on Mon Mar  6 19:37:10 2023
 import sys
 sys.path.append("C:\\Users\\cwilson\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python39\\site-packages")
 sys.path.append('C:\\Users\\cwilson\\documents\\python')
+sys.path.append('C:\\Users\\cwilson\\documents\\python\\Speedo_Dashboard')
 from Pull_Fablisting_data import get_fablisting_plus_model_summary
 from Pull_TimeClock_data import get_timeclock_summary
 from Post_to_GoogleSheet import post_observation, post_predictor
-
+import datetime
 # this will be the controller for automation?
 
+state = 'TN'
+sheet = 'CSM QC Form' 
 
-fablisting_summary = get_fablisting_plus_model_summary()
-timeclock_summary = get_timeclock_summary()
+now = datetime.datetime.now()
+start_dt = now
+if start_dt.hour < 6:
+    start_dt = start_dt - datetime.timedelta(days=1)
+    start_dt = start_dt.replace(hour=6, minute=0, second=0, microsecond=0)
+else:
+    start_dt = now.replace(hour=0, minute=0, second=0, microsecond=0)
+
+end_dt = start_dt + datetime.timedelta(days=1)
+
+
+
+fablisting_summary = get_fablisting_plus_model_summary(start_dt, end_dt, sheet=sheet)
+timeclock_summary = get_timeclock_summary(start_dt, end_dt, state=state, basis=None)
 
 gsheet_dict = {}
 gsheet_dict.update(fablisting_summary)
