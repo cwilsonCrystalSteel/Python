04c199e45d5473526928fbcee2e0316efd94a64c
cwilsonCrystalSteel <127119558+cwilsonCrystalSteel@users.noreply.github.com>
2023-03-21 13:15:57 -0500

    changes bc of looker

--- Speedo_Dashboard/Automated_SpeedoController.py
+++ Speedo_Dashboard/Automated_SpeedoController.py
@@ -28,7 +28,7 @@ while start_dt.weekday() != 0:
 
 end_dt = start_dt + datetime.timedelta(days=6)
 end_dt = end_dt.replace(hour=23, minute=59)
-
+print('running the speedo dashboard for {} to {}'.format(start_dt, end_dt))
 # start_dt = now
 # if start_dt.hour < 6:
 #     start_dt = start_dt - datetime.timedelta(days=1)
@@ -61,16 +61,16 @@ predictor = None
 #     post_predictor()
 
 
-if now.hour == 6:
-    ''' get the previous days shit for the summary tab '''
-    start_dt = now - datetime.timedelta(days=1)
-    start_dt = start_dt.replace(hour=6, minute=0, second=0, microsecond=0)
-    end_dt = start_dt + datetime.timedelta(days=1)
-    fablisting_summary = get_fablisting_plus_model_summary(start_dt, end_dt, sheet=sheet)
-    timeclock_summary = get_timeclock_summary(start_dt, end_dt, state=state, basis=None)
+# if now.hour == 6:
+#     ''' get the previous days shit for the summary tab '''
+#     start_dt = now - datetime.timedelta(days=1)
+#     start_dt = start_dt.replace(hour=6, minute=0, second=0, microsecond=0)
+#     end_dt = start_dt + datetime.timedelta(days=1)
+#     fablisting_summary = get_fablisting_plus_model_summary(start_dt, end_dt, sheet=sheet)
+#     timeclock_summary = get_timeclock_summary(start_dt, end_dt, state=state, basis=None)
     
-    run_for_date = start_dt.strftime('%m/%d/%Y')
-    gsheet_dict = {'Date':run_for_date}
-    gsheet_dict.update(fablisting_summary)
-    gsheet_dict.update(timeclock_summary)    
-    post_observation(gsheet_dict, isReal=True, sheet_name='Day Summary')
+#     run_for_date = start_dt.strftime('%m/%d/%Y')
+#     gsheet_dict = {'Date':run_for_date}
+#     gsheet_dict.update(fablisting_summary)
+#     gsheet_dict.update(timeclock_summary)    
+#     post_observation(gsheet_dict, isReal=True, sheet_name='Day Summary')
