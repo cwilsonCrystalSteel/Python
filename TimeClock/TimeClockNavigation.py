094da49e5527768234a5fc47df128dd0de44ed3e
cwilsonCrystalSteel <127119558+cwilsonCrystalSteel@users.noreply.github.com>
2024-08-02 11:01:18 -0500

    new timeclock grabber for employee locations is fire

--- TimeClock/TimeClockNavigation.py
+++ TimeClock/TimeClockNavigation.py
@@ -8,13 +8,15 @@ Created on Thu Aug  1 15:36:05 2024
 import time
 import datetime
 import os
+import glob
+import datetime
+from TimeClock_Credentials import returnTimeClockCredentials
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from TimeClock_Credentials import returnTimeClockCredentials
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, ElementNotInteractableException
 
@@ -55,54 +57,66 @@ def newest_creation_time(download_folder):
     return newest_file_ctime
 
 
+
+def validateElement(driver, findElementArg, elemOutputName, checkPresence=True, checkClickable=True, timeoutLimit=10):
+    if checkPresence:
+        try:
+            elem = WebDriverWait(driver, timeoutLimit).until(EC.presence_of_element_located(findElementArg))
+            print(f'The element is present:   {elemOutputName}')
+        except TimeoutException:
+            print(f'Timeout Error for presence: {elemOutputName}')  
+    
+    if checkClickable:
+        try:
+            elem = WebDriverWait(driver, timeoutLimit).until(EC.element_to_be_clickable(findElementArg))
+            print(f'The element is clickable: {elemOutputName}')
+        except TimeoutException:
+            print(f'Timeout Error for clickability: {elemOutputName}')  
+            
+    return elem
+
+
+
 #%%
 class TimeClockBase():
-    def __init__(self, download_folder='c:\\users\\cwilson\\downloads', headless=True, fullscreen=False):
+    def __init__(self, download_folder='c:\\users\\cwilson\\downloads\\', headless=True, fullscreen=False):
         
         self.creds = returnTimeClockCredentials()
         self.download_folder = download_folder
         self.headless = headless
         self.fullscreen = fullscreen
         
-        
-            
-            
         if self.fullscreen and self.headless:
             Exception('Both options fullscreen & headless are enabled, only one can be enabled at a time')
-            
+          
+        # init the service?
         self.service = Service()
-            
-            
+        
+        # set options for headless browser or not
         if self.headless:
             print('Browser will be HEADLESS')
             options = setting_chrome_options(True)
         else:
             options = setting_chrome_options(False)
             
-            
+        # init dirver with headless/options
         self.driver = webdriver.Chrome(service=self.service, options = options)
+        print('Driver created...')
             
-            
+        # go fullscreen - when not headless!
         if self.fullscreen:  
             print('Browser will be FULLSCREEN')
             # full screen cus this website sucks
             self.driver.maximize_window()
             
-        
-        
-        
+        # create download folder if not exists
         
         if os.path.exists(download_folder):
             enable_download(self.driver, self.download_folder)
         else:
             os.makedirs(download_folder)
             enable_download(self.driver, self.download_folder)
-            
-        print('Driver created...')
-        
-        
-        
-        
+   
         
         
     def kill(self):
@@ -120,47 +134,25 @@ class TimeClockBase():
             
     def startupBrowser(self):
         self.splashPage()
-        # printwait('Loading website', 6)
-        try:
-            self.usernameInput = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'LogOnUserId')))
-            print('The login page was loaded in a timely manner - username box')
-        except TimeoutException:
-            print('Loading the login page took too long!')
-            
-        try:
-            self.passwordInput = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'LogOnUserPassword')))
-            print('The login page was loaded in a timely manner - passwordInput box')
-        except TimeoutException:
-            print('Loading the login page took too long!')     
-            
-    def loginInteractable(self):
         
-        try:
-            self.usernameInput = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'LogOnUserId')))
-            print('The login page usernameInput box is clickable')
-        except TimeoutException:
-            print('Loading the login page usernameInput is not clickable!')  
+        self.usernameInput = validateElement(self.driver, (By.ID, 'LogOnUserId'), 'usernameInput', checkPresence=True, checkClickable=True)
+        self.passwordInput = validateElement(self.driver, (By.ID, 'LogOnUserPassword'), 'passwordInput', checkPresence=True, checkClickable=True)
             
-        try:
-            self.passwordInput = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'LogOnUserPassword')))
-            print('The login page passwordInput box is clickable')
-        except TimeoutException:
-            print('Loading the login page passwordInput is not clickable!')         
         
     def login(self):
         # try and remove anything in the text boxes
         delete_range(self.usernameInput)
         delete_range(self.passwordInput)
         
-        # usernameInput = self.driver.find_element(By.ID, 'LogOnUserId')
-        # get the username & password dict
+        # enter the username
         self.usernameInput.send_keys(self.creds['username'])
-        printwait('Entered Username', 0)
-        # Find the password field
-        # password = self.driver.find_element(By.ID, 'LogOnUserPassword')
-        # Submit password
+        print('Entered Username')
+        
+        
+        # enter password
         self.passwordInput.send_keys(self.creds['password'])
-        printwait('Entered Password', 0)
+        print('Entered Password')
+        
         # Press 'enter' to login
         self.passwordInput.send_keys(Keys.RETURN)
         
@@ -168,7 +160,6 @@ class TimeClockBase():
     def tryLogin(self):
         
         try:
-            self.loginInteractable()
             self.login()
         except ElementNotInteractableException:
             print('Elements of login() are not Interactable at this time, retrying startup..')
@@ -179,34 +170,23 @@ class TimeClockBase():
         if not self.driver.current_url == 'https://136509.tcplusondemand.com/app/manager/#/ManagerLogOn/136509':
             Exception('Cannot begin to open Tabular Menu b/c the URL is not correct')
         
-        try:
-            self.tabularMenu = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'HeaderMenuIcon')))
-            print('Found Tabular Menu')
-        except TimeoutException:
-            print('Timeout Exception for the Tabular Menu') 
+        self.tabularMenu = validateElement(self.driver, (By.CLASS_NAME, 'HeaderMenuIcon'), 'tabularMenu', checkPresence=True, checkClickable=True)
         
-        # self.tabularmenu = self.driver.find_element(By.CLASS_NAME, 'HeaderMenuIcon')
-        # printwait('Found Tabular menu', 3) 
+        # click the tabular Menu
         self.tabularMenu.click()
+        print('Tabular Menu Clicked on...')
         
         
     def searchFromTabularMenu(self, searchText=''):
         # group hours:              searchText = 'Group Hours'
         # employee information      searchText = 'export'
         
-        
-        try:
-            self.tabularMenuSearchBox = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'SearchInput')))
-            print('Found Search Box')
-        except TimeoutException:
-            print('Timeout Exception for the Tabular Menu Search Box') 
+        self.tabularMenuSearchBox = validateElement(self.driver, (By.CLASS_NAME, 'SearchInput'), 'tabularMenuSearchBox', checkPresence=True, checkClickable=True)
         
         
-        # printwait('Found search box', 3) 
-        # self.searchbox = self.driver.find_element(By.CLASS_NAME, 'SearchInput')
         self.tabularMenuSearchBox.send_keys(searchText)
         self.tabularMenuSearchBox.send_keys(Keys.RETURN)
-        printwait(f'Searched "{searchText}"', 0) 
+        print(f'Searched "{searchText}"') 
         
     def clickTabularMenuSearchResults(self, findText=''):
         # group hours:              findText = 'Hours > Group Hours'
@@ -214,23 +194,101 @@ class TimeClockBase():
         
         self.clickTabularMenuSearchXPATH = f"//*[contains(text(), '{findText}')]"
         
-        print(f'Attempting to find by.XPATH: {self.clickTabularMenuSearchXPATH}')
+        # print(f'Attempting to find by.XPATH: {self.clickTabularMenuSearchXPATH}')
         
-        try:
-            self.targetedItem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, self.clickTabularMenuSearchXPATH)))
-            print(f'Found {findText}')
-        except TimeoutException:
-            print(f'Timeout Exception for locating {findText}') 
+        self.targetedItem = validateElement(self.driver, (By.XPATH, self.clickTabularMenuSearchXPATH), findText, checkPresence=True, checkClickable=True)
             
-        # find group hours link
-        # self.targetedItem = self.driver.find_element(By.XPATH, "//*[contains(text(), '{findText}')]")
+        # Click on the desired Group Hours or Employee Information
         self.targetedItem.click()
-        printwait(f'Clicked "{findText}"', 0)
+        print(f'Clicked "{findText}"')
         
         
-    # def employeeLocationFinale(self):
+    def employeeLocationFinale(self, exclude_terminated=False):
+        # we want a time to base our download window from
+        self.startTime = datetime.datetime.now()
+        
+        self.exportType = validateElement(self.driver, (By.ID, 'selExportType'), 'exportType', checkPresence=True, checkClickable=True)
+        
+        # creates a selectable(?) from the dropdown
+        selectedExportType = Select(self.exportType)
+        # this is much better approach then typing in the text and hoping for the best
+        selectedExportType.select_by_visible_text('Employee Information')
+        print('Found Export Type: Employee Information')
+        
+        # Navigate to Export Templates
+        self.exportTemplatesDropdown = validateElement(self.driver, (By.XPATH, "//*[contains(text(), 'Export Templates')]"), 'exportTemplatesDropdown', checkPresence=True, checkClickable=True)
+        self.exportTemplatesDropdown.click()
+        print('Found Export Templates')
+        
+        # clicks on my custom report called "emplyee locations"
+        self.employeeLocationsDropdownItem = validateElement(self.driver, (By.XPATH, "//*[contains(text(), 'employee location')]"), 'employeeLocationsDropdownItem', checkPresence=True, checkClickable=True)
+        self.employeeLocationsDropdownItem.click()
+        print('Found custom tempalte: employee locations')
         
         
+        if exclude_terminated == False:
+            
+            
+            self.employeeFiltersMenu = validateElement(self.driver, (By.XPATH, "//input[@value='Employee Filter']"), 'employeeFiltersMenu', checkPresence=True, checkClickable=True)
+            self.employeeFiltersMenu.click()
+            print('Found employee filter button')
+            
+            self.excludeSuspendedButton = validateElement(self.driver, (By.XPATH, "//input[@id='chkExcludeSuspended']"), 'excludeSuspendedButton', checkPresence=True, checkClickable=True)              
+            # is_selected() = True if the box is checked to exclude suspended employees
+            if self.excludeSuspendedButton.is_selected():
+                self.excludeSuspendedButton.click()
+                print('Un-checked the Exclude Suspended checkbox')
+            
+            
+            self.excludeTerminatedButton = validateElement(self.driver, (By.XPATH, "//input[@id='chkExcludeTerminated']"), 'excludeTerminatedButton', checkPresence=True, checkClickable=True)               
+            if self.excludeTerminatedButton.is_selected():
+                self.excludeTerminatedButton.click()
+                print('Un-checked the Exclude Terminated checkbox')
+        
+        
+        
+            self.submitFilterButton = validateElement(self.driver, (By.XPATH, "//input[@value='Filter']"), 'submitExclusionsFilterButton', checkPresence=True, checkClickable=True)               
+            self.submitFilterButton.click()
+            print('Filtered the suspended & terminated')
+        
+        
+        self.generateButton = validateElement(self.driver, (By.XPATH, "//input[@value='Generate']"), 'generateButton', checkPresence=True, checkClickable=True)               
+        self.generateButton.click()
+        print('Found Generate')
+        
+        self.downloadButton = validateElement(self.driver, (By.XPATH, "//input[@value='Download']"), 'downloadButton', checkPresence=True, checkClickable=True)               
+        self.downloadButton.click()
+        print('Began File Download')       
+        
+        
+    def retrieveDownloadedFile(self, waitTime, fileType, searchText):
+        # Group Hours:              fileType = '*.html'
+        #                           searchText = 'Hours'
+        # Employee Information:     fileType = '*.csv'
+        #                           searchText = 'Employee Information'
+        
+        print(f"Searching for {fileType} with text {searchText} in {self.download_folder}")
+        endTime = time.time() + waitTime
+        while True:
+            
+            self.listOfFileTypes = glob.glob(self.download_folder + fileType) # * means all if need specific format then *.csv
+            # Create a list with only the states we want to look at
+            self.foundFiles = [f for f in self.listOfFileTypes if searchText in f]
+            # Get the most recent file for that state
+            self.newestFile = max(self.foundFiles, key=os.path.getctime)
+            # get the file creation time as a datetime
+            self.newestFileTime = datetime.datetime.fromtimestamp(os.path.getctime(self.newestFile))
+            
+            if self.newestFileTime > self.startTime:
+                print(f'File found: {self.newestFile}')
+                return self.newestFile
+            
+            if time.time() > endTime:
+                raise Exception('No reasonable download was found...')
+            
+        
+            
+            
         
         
 x = TimeClockBase(headless=False)     
@@ -239,8 +297,13 @@ x.tryLogin()
 x.openTabularMenu()
 x.searchFromTabularMenu('export')
 x.clickTabularMenuSearchResults('Tools > Export')
+x.employeeLocationFinale()
+filepath = x.retrieveDownloadedFile(10, '*.csv', 'Employee Information')
+
 # x.searchFromTabularMenu('Group Hours')
 # x.clickTabularMenuSearchResults('Hours > Group Hours')
+# x.groupHoursFinale()
+# filepath = x.retrieveDownloadedFile(10, '*.html', 'Hours')
 
 
   #%%      
