4cf7181b123cf38beb6ccbf5f476d56faf2cbd3e
cwilsonCrystalSteel <127119558+cwilsonCrystalSteel@users.noreply.github.com>
2024-10-11 17:02:50 -0500

    calendar 'fix'
    idk whats up with manilla calendar

--- find_string_within_python_directory.py
+++ find_string_within_python_directory.py
@@ -48,4 +48,4 @@ def find_string_in_the_dir(string_to_find):
                         specific[file].append(i)
                 
 
-find_string_in_the_dir("download_most_current_employee_location_csv")
+find_string_in_the_dir("No open sequences")
