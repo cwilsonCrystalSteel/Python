30a3d9b9b5b4719c643745e78cc8a2e002b6a2a6
cwilsonCrystalSteel <127119558+cwilsonCrystalSteel@users.noreply.github.com>
2025-03-28 18:10:29 -0400

    start doing eva in the db

--- Get_model_estimate_hours_attached_to_fablisting.py
+++ Get_model_estimate_hours_attached_to_fablisting.py
@@ -10,9 +10,8 @@ import datetime
 import glob
 import os
 import numpy as np
-from Grab_Fabrication_Google_Sheet_Data import grab_google_sheet
-from navigate_EVA_folder_function import get_df_of_all_lots_files_information
 import sys
+from navigate_EVA_folder_function import get_df_of_all_lots_files_information
 from utils.attendance_google_sheets_credentials_startup import init_google_sheet as init_google_sheet_production_worksheet
 from Speedo_Dashboard.Post_to_GoogleSheet import get_production_worksheet_job_hours
 
---
+++ Google/pullEVALotsLogFromGoogle.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 28 17:09:44 2025
+
+@author: Netadmin
+"""
+
+from utils.attendance_google_sheets_credentials_startup import init_google_sheet as init_google_sheet_production_worksheet
+import pandas as pd
+
+
+def fix_cols(columns_list):
+    # init a dict
+    new_cols_dict = {}
+    # iterate thru each column
+    for col in columns_list:
+        # replace any newlines with a space
+        new_col = col.replace('\n', ' ')
+        # make a dict entry of key = old, value = new
+        new_cols_dict[col] = new_col
+        
+    # return dict to do pandas rename
+    return new_cols_dict
+
+def pullEVALotsLogFromGoogle(drop_nonnumeric=True):
+    _ProductionWorksheetGooglekey = "1HIpS0gbQo8q1Pwo9oQgRFUqCNdud_RXS3w815jX6zc4"
+
+    sh = init_google_sheet_production_worksheet(_ProductionWorksheetGooglekey)
+    # get the values from the shipping schedule as a list of lists
+    worksheet = sh.worksheet('LOTS Log').get_all_values()
+    
+    ll = pd.DataFrame(worksheet[3:], columns=worksheet[2])    
+    # get rid of line breaks in the column names
+    ll = ll.rename(columns=fix_cols(ll.columns))
+
+    # only keep what we need
+    ll = ll[['Job','Fabrication Site','LOTS Name','Tonnage','TOTAL MHRS']]
+    # rename the earned hours
+    ll = ll.rename(columns={'TOTAL MHRS':'LOT EVA Hours'})
+    # force conversion to a number
+    ll['LOT EVA Hours'] = pd.to_numeric(ll['LOT EVA Hours'], errors='coerce')
+    # make tonnage a number
+    ll['Tonnage'] = pd.to_numeric(ll['Tonnage'], errors='coerce')
+    if drop_nonnumeric:
+        # get rid of anything that doesnt work to be forced into a number
+        ll = ll[~ll['LOT EVA Hours'].isna()]
+        # drop any unavailable tonnages 
+        ll = ll[~ll['Tonnage'].isna()]
+        
+    # calcualte poundage of LOT horus
+    ll['LOT EVA per lb'] = ll['LOT EVA Hours'] / (ll['Tonnage'] * 2000)
+    return ll
+
+    
+    
